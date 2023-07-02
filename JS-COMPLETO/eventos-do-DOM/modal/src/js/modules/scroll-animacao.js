1ce56daa5acb443680e1b068d6e605e1741340ec
Breno Teixeira <brenotm10@gmail.com>
2023-07-01 22:14:33 -0300

    estudos

---
+++ JS-COMPLETO/eventos-do-DOM/modal/src/js/modules/scroll-animacao.js
@@ -0,0 +1,43 @@
+export default function initAnimacaoScroll(){
+  const sections = document.querySelectorAll('[data-anime="sroll"]');
+  
+  if(sections.length){
+    
+   
+    const windowMetade = window.innerHeight * 0.6;
+    
+  
+    function animaScroll(){
+      sections.forEach((section)=>{
+
+        
+        const sectionTop = section.getBoundingClientRect().top;
+        const isSectionVisible = (sectionTop - windowMetade) < 0;
+        
+        
+        if(isSectionVisible){
+          section.classList.add('ativo');
+        } else {
+          section.classList.remove('ativo');
+        }
+      });
+    };
+    animaScroll();
+    
+    window.addEventListener('scroll', animaScroll)
+  };
+};
+
+initAnimacaoScroll()
+  
+  
+
+
+
+
+
+
+
+
+
+
