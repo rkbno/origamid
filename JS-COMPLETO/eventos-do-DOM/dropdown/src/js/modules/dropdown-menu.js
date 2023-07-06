0182e03df811ebe48440a6cab8f10e14a57c89bc
Breno Teixeira <brenotm10@gmail.com>
2023-07-06 16:52:17 -0300

    estudos

--- JS-COMPLETO/eventos-do-DOM/dropdown/src/js/modules/dropdown-menu.js
+++ JS-COMPLETO/eventos-do-DOM/dropdown/src/js/modules/dropdown-menu.js
@@ -1,3 +1,33 @@
 export default function initDropdownMenu(){
+
 }
 
+const dropdownMenus = document.querySelectorAll('[data-dropdown]');
+
+dropdownMenus.forEach(menu => {
+  ['touchstart', 'click'].forEach(userEvent => {
+    menu.addEventListener(userEvent, handleClick);
+  });
+});
+
+function handleClick(event){
+  event.preventDefault();
+  this.classList.add('active');
+  outsideClick(this,() => {
+    this.classList.remove('active');
+  }); 
+};
+
+
+function outsideClick(element, callback){
+  
+  const html = document.documentElement;
+
+
+  html.addEventListener('click', handleOutsideClick);
+  function handleOutsideClick(event){
+   if (!element.contains(event.target))
+ 
+    callback();
+  }
+};
