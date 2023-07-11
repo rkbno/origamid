30b21a804d23350d19ae9182aa4bee9db3e430e5
Breno Teixeira <brenotm10@gmail.com>
2023-07-11 11:29:18 -0300

    estudos

--- JS-COMPLETO/eventos-do-DOM/anima-numeros/src/js/modules/anima-numeros.js
+++ JS-COMPLETO/eventos-do-DOM/anima-numeros/src/js/modules/anima-numeros.js
@@ -1,35 +1,34 @@
 export default function initAnimaNumeros(){
- 
+  function animaNumeros (){
+    const numeros = document.querySelectorAll('[data-numero]');
+  
+    numeros.forEach((numero) => {
+      const total = +numero.innerText;
+      let start = 0;
+      
+      const timer = setInterval(()=>{
+      const incremento = Math.floor(total / 100);  
+        start = start + incremento;
+        numero.innerText = start;
+        if(start > total) {
+          numero.innerText = total;
+          clearInterval(timer);
+        };
+      }, 25 * Math.random());
+    });
+  };
+  function handleMutation(mutation){
+    if( mutation[0].target.classList.contains('ativo'));
+    observer.disconnect();
+    animaNumeros()
+  };
+  const observerTarget = document.querySelector('.numeros');
+  const observer = new MutationObserver(handleMutation);
+  
+  observer.observe(observerTarget, {attributes: true});
 };
 
-function animaNumeros (){
-  const numeros = document.querySelectorAll('[data-numero]');
 
-  numeros.forEach((numero) => {
-    const total = +numero.innerText;
-
-    let start = 0;
-    const timer = setInterval(()=>{
-    const incremento = Math.floor(total / 100);  
-      start = start + incremento;
-      numero.innerText = start;
-      if(start > total) {
-        numero.innerText = total;
-        clearInterval(timer);
-      }
-    }, 25 * Math.random());
-  });
-};
-function handleMutation(mutation){
-  if( mutation[0].target.classList.contains('ativo'));
-  observer.disconnect();
-  animaNumeros()
-};
-
-const observerTarget = document.querySelector('.numeros');
-const observer = new MutationObserver(handleMutation);
-
-observer.observe(observerTarget, {attributes: true});
 
 
 
